8ceefe8fea5cc5acd6c9c298b5d0c65e179b71a2
Huy Pham <hdpham1@uci.edu>
2016-07-02 20:10:07 -0700

    added .jar file

--- server.java
+++ server.java
@@ -3,30 +3,26 @@ import java.net.*;
 import java.io.*;
 
 public class server {
+	public static Process p;
 	
 	public static void execute_stream(String url) throws IOException, InterruptedException
 	{
 		Runtime r = Runtime.getRuntime();
 		String i = String.format(
-				"livestreamer --player=omxplayer --fifo %1 best =ringbuffer-size=\"4M\"", url);
+				"livestreamer --player=omxplayer --fifo %s best -ringbuffer-size=\"4M\"", url);
 		System.out.println(i);
-//		Process p = r.exec(String.format(
-//				"livestreamer --player=omxplayer --fifo %1 best =ringbuffer-size=\"4M\"", url));
-//		p.waitFor();
-//		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//		String line = "";
-//
-//		while ((line = b.readLine()) != null) {
-//		  System.out.println(line);
-//		}
-		
+		p = r.exec("livestreamer --player=omxplayer --fifo " + url + " best -ringbuffer-size=\"4M\"");
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+		System.out.println("done");	
 	}
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
 		if (args.length != 1) {
-            System.err.println("Usage: java KnockKnockServer <port number>");
-            System.exit(1);
-        }
+            		System.err.println("Usage: java KnockKnockServer <port number>");
+            		System.exit(1);
+        	}
 
         int portNumber = Integer.parseInt(args[0]);
         InetAddress ip = InetAddress.getLocalHost();
@@ -41,34 +37,33 @@ public class server {
                 new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()));
-        		){
-        	System.out.println("Ready.");
+        		)
+		{
+        		System.out.println("Ready.");
         	
-        	String inputLine;
-        	String outputLine = "Ready";
-        	out.println(outputLine);
-        	while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine);
-                outputLine = "Now Playing: " + inputLine;
-                out.println(outputLine);
-                try{
-                	execute_stream(inputLine);
-                }
-                catch (InterruptedException e)
-                {
-                	System.out.println("Failed to execute command.");
-                }
-                if (inputLine.equals("stop"))
-                    ;
-                else if (inputLine.equals("quit"))
-                	break;
-            }
+        		String inputLine;
+        		String outputLine = "Ready.";
+        		out.println(outputLine);
+        		while ((inputLine = in.readLine()) != null) {
+                		if (inputLine.equals("stop")){
+					p.destroy();
+                			continue;
+				}
+                		else if (inputLine.equals("quit"))
+                			break;
+                		System.out.println(inputLine);
+                		outputLine = "Now Playing: " + inputLine;
+                		out.println(outputLine);
+                		try{
+                			execute_stream(inputLine);
+                		} catch (InterruptedException e) {
+                			System.out.println("Failed to execute command.");
+                		}
         	
-        }
-        catch(IOException e)
-        {
+        		}
+		} catch(IOException e) {
         
-        }
+       	  	}
 	}
 
 }
